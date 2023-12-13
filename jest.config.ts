454701362b6a7330473345f6885dead0212bd122
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2023-12-12 22:31:07 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\ts$': 'ts-jest'
+  },
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src']
+}
+
+export default config
