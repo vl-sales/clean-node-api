faa4cb12d7aaec52e77a2a4e4cd49583f85e1926
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2023-12-14 11:03:20 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingPramError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/https-helper'
+import { type Controller } from '../protocols/controller'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
