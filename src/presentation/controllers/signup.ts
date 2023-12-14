80385b59f280d3348e0e52385d0c581ffdbeeaed
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2023-12-14 09:00:56 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { type HttpRequest, type HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingPramError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingPramError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingPramError(field))
+      }
     }
   }
 }
