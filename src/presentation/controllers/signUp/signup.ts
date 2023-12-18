b6eedf8320f5d4023aa24320259c434df5b182e6
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2023-12-18 08:48:26 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/signUp/signup.ts
+++ src/presentation/controllers/signUp/signup.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/https-helper'
+import { badRequest, ok, serverError } from '../../helpers/https-helper'
 import { type HttpRequest, type HttpResponse, type Controller, type EmailValidator, type AddAccount } from './signup-protocols'
 
 export class SignUpController implements Controller {
@@ -35,10 +35,7 @@ export class SignUpController implements Controller {
         password
       })
 
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
