24de92527db8e13a2cfba50d5e9293411bf9c3fa
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-01-18 23:38:33 -0300

    feat: ensure signup routes returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
+import setupRoutes from './routes'
 
 const app = express()
 export default app
 setupMiddleware(app)
+setupRoutes(app)
---
+++ src/main/config/routes.ts
@@ -0,0 +1,11 @@
+import { type Express, Router } from 'express'
+import fg from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  fg.sync('**/src/main/routes/**routes.ts').map(async (file) => {
+    (await import(`../../../${file}`)).default(router)
+  })
+
+  app.use('/api', router)
+}
