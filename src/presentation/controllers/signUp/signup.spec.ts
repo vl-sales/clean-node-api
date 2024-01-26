d7f4f54e305cfbb66de8708cd6ba9334b3688745
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-01-26 09:01:16 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -168,7 +168,7 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(new Error().stack))
   })
 
   test('Should call AddAccount with correct values', async () => {
@@ -208,7 +208,7 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(new Error().stack))
   })
 
   test('Should return 200 if valid data is provided', async () => {
