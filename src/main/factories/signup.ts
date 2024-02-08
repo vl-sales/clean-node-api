0b0f342e46a33603cb4ff31cb11664962dff739b
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-02-08 08:49:23 -0300

    feat: add LogMongoRepository to a factory method

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signUp/signup'
 import { type Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -15,5 +16,6 @@ export const makeSignupController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
 
   const signupController = new SignUpController(emailValidator, addAccount)
-  return new LogControllerDecorator(signupController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signupController, logMongoRepository)
 }
