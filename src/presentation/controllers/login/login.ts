b537dc8cc0b9d1a973f7fe339a9f22df4abd8d52
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-02-08 21:13:21 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,14 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/https-helper'
 import { type Controller, type HttpRequest, type HttpResponse } from '../../protocols'
+import { type EmailValidator } from '../signUp/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body?.email) {
       return badRequest(new MissingParamError('email'))
@@ -10,5 +16,7 @@ export class LoginController implements Controller {
     if (!httpRequest.body?.password) {
       return badRequest(new MissingParamError('password'))
     }
+
+    this.emailValidator.isValid(httpRequest.body?.email)
   }
 }
