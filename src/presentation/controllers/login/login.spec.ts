009fca04e799b1a643d8e62e0b24639a29ca6ffd
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-02-08 21:01:04 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,10 +2,21 @@ import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/https-helper'
 import { LoginController } from './login'
 
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+
+  return {
+    sut
+  }
+}
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
-
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -18,7 +29,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
