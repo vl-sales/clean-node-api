51cfb58e3fd3420de07388ad88543c565d1f8acd
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-02-09 07:46:04 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { LoginController } from './login'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/https-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/https-helper'
 import { type HttpRequest, type EmailValidator, type Authentication } from './login-protocols'
 
 const makeEmailValidator = (): EmailValidator => {
@@ -132,5 +132,12 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toEqual(serverError(new Error('unit test')))
+  })
+
+  test('Should return 200 if valid credentials are provided', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 })
