6ce7b7837dd17d0fa896067cbde457eed35362d4
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-02-28 08:32:28 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -42,4 +42,11 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error(''))
   })
+
+  test('Should note return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })
