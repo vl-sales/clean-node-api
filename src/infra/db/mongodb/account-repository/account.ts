0f6a025a2a08e3422faa1551f2725b397f97313b
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-03-06 08:32:03 -0300

    refactor: move data layer interfaces to folders

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,4 +1,4 @@
-import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/useCases/add-account'
 import { type DefaultFields } from '../../protocols/defaultFields'
