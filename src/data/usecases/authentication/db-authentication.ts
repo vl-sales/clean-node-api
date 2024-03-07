02a269db969474f1b5d0aad42a7fd5ef5d40fe33
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-03-07 08:23:19 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,19 +2,23 @@ import { type Authentication, type AuthenticationModel } from '../../../domain/u
 import { type HashComparer } from '../../protocols/criptography/hash-comparer'
 import { type TokenGenerator } from '../../protocols/criptography/token-generator'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -22,7 +26,9 @@ export class DbAuthentication implements Authentication {
     if (account) {
       const passwordIsValid = await this.hashComparer.compare(authentication.password, account.password)
       if (passwordIsValid) {
-        return await this.tokenGenerator.generate(account.id)
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
+        return accessToken
       }
     }
     return null
