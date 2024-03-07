072d7d0cbaa75f1f062eb54ff62918a55cc93261
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-03-07 08:30:56 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,8 +1,11 @@
-import { type Authentication, type AuthenticationModel } from '../../../domain/useCases/authentication'
-import { type HashComparer } from '../../protocols/criptography/hash-comparer'
-import { type TokenGenerator } from '../../protocols/criptography/token-generator'
-import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import {
+  type Authentication,
+  type AuthenticationModel,
+  type HashComparer,
+  type TokenGenerator,
+  type LoadAccountByEmailRepository,
+  type UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
