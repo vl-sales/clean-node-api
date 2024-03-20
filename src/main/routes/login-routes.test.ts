adf15d985ec0c7ac281f79f527bdd900c8435555
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-03-19 22:56:34 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,14 +1,17 @@
 import request from 'supertest'
 import app from '../config/app'
+import { type Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { hash } from 'bcrypt'
 
+let accountCollection: Collection
 describe('SignUp routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +28,24 @@ describe('SignUp routes', () => {
           email: 'lande0600@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on success', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Vinicius',
+        email: 'lande0600@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lande0600@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
