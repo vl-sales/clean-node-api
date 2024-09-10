d4163cba4f8d660498d3faa919fd3d43b55bb798
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-09-09 22:18:04 -0300

    test: ensure DbLoadAccountByToken throws if any dependecy throws

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -86,4 +86,20 @@ describe('DbLoadAccountByToken UseCase', () => {
 
     expect(account).toEqual(makeAccount())
   })
+
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const promise = sut.load('any_token')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if LoadAccountByTokenRepository throws', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const promise = sut.load('any_token')
+    await expect(promise).rejects.toThrow()
+  })
 })
