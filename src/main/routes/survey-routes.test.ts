46726f63a1f6592df9817956da2d0ccf3414fc8b
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-11-24 11:38:21 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { type Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 describe('SignUp routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -12,6 +15,8 @@ describe('SignUp routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -22,6 +27,37 @@ describe('SignUp routes', () => {
     test('Should return 403 if no access token is provided', async () => {
       await request(app)
         .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'Answer 1',
+              answer: 'http://image-name.com'
+            },
+            {
+              answer: 'Anwer 2'
+            }
+          ]
+        })
+        .expect(403)
+    })
+  })
+
+  describe('POST /survey', () => {
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Vinicius',
+        email: 'lande0600@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = result.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-accesss-token', accessToken)
         .send({
           question: 'Question',
           answers: [
