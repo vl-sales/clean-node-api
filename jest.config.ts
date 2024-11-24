9bdf3f61b93776ad4881d46f81b4484d12367ba6
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-11-24 11:58:49 -0300

    chore: remove import files from jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -7,16 +7,11 @@ const config: Config = {
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/**/protocols/*',
     '!<rootDir>/src/**/protocols/*',
-    '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
     '!<rootDir>/src/domain/models/**',
     '!<rootDir>/src/domain/useCases/**',
-    '!<rootDir>/src/presentation/controllers/signUp/signup-protocols.ts',
-    '!<rootDir>/src/presentation/controllers/login/login-protocols.ts',
     '!<rootDir>/src/presentation/helpers/validators/validation.ts',
     '!<rootDir>/src/data/protocols/**',
-    '!<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts',
-    '!<rootDir>/src/presentation/controllers/login/login-controller-protocols.ts',
-    '!<rootDir>/src/data/usecases/add-survey/db-add-survey-protocols.ts'
+    '!<rootDir>/src/**/*-protocols.ts'
   ],
   collectCoverage: true,
   coverageDirectory: 'coverage',
