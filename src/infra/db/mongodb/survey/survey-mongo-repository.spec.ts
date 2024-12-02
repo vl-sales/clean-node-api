db78d6c8eadbe85e9a2f883c68621121ce1f9de9
Vinicius L Sales <vinicius.sales@stratesys-ts.com>
2024-12-02 14:17:40 -0300

    test: ensure SurveyMongoRepository returns empty list

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -75,5 +75,12 @@ describe('Account Mongo Repository', () => {
       expect(surveys[0].question).toBe('Any_question')
       expect(surveys[1].question).toBe('other_question')
     })
+
+    test('Should load empty list', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+
+      expect(surveys.length).toBe(0)
+    })
   })
 })
